fa5e86eaa1bf3361599d07b4c98b49cb26850eb3
Sophia Wassermann <hi@sowasser.com>
2021-02-09 19:46:49 +0000

    Started with means for dungeness crab & looking at ports fishing salmon

--- CA_port_landings_species.R
+++ CA_port_landings_species.R
@@ -4,13 +4,35 @@
 # Specifically, this script is for the landings data by port, listed as:
 # "Landing Receipt Records: Landings Data By Port 1992-2014"
 
+library(dplyr)
+library(tidyr)
 library(reshape2)
-library(ggplot2)
-library(viridis)
-library(patchwork)
 
-port_landings_all <- read.csv("Data/port_landings_92-14.csv")
-# Remove non-applicable columns
-port_landings <- port_landings_all[, -c(1, 5, 6, 10)] 
+port_landings <- read.csv("Data/port_landings_92-14.csv")
 
 species <- levels(factor(port_landings$fishery))  # list of species
+
+# Workflow for Dungeness Crab -------------------------------------------------
+# Select only the dungeness crab data
+crab_all <- port_landings %>% filter(fishery == "DUNGENESS CRAB")
+crab <- crab_all %>% drop_na()
+
+# NB: when finding the means, this aggregates different gear types.
+# Find means by year
+crab_yr_mean <- group_by(crab, year) %>% summarize(m= mean(ex.vessel_revenue))
+
+# Find means by year & port
+crab_yr_port_mean <- group_by(crab, year, port) %>% summarize(m= mean(ex.vessel_revenue))
+
+
+# Workflow for salmon closure -------------------------------------------------
+# Select salmon data
+salmon_all <- port_landings %>% filter(fishery == "SALMON")
+salmon_ports <- levels(factor(salmon_all$port))  # ports where salmon was fished
+
+# Select all data for ports where salmon was fished at some point
+sal_port_data <- filter(port_landings, port %in% salmon_ports)
+sal_port_data <- sal_port_data %>% drop_na()
+
+# Find count of number of fisheries for salmon-catching ports by year
+sal_port_count <- group_by(crab, year, port) %>% summarize(m= mean(ex.vessel_revenue))

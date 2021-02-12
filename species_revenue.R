d3d14d8a3d163591b49c92a1ee6bd6a1f6439de5
Sophia Wassermann <hi@sowasser.com>
2021-02-12 22:33:11 +0000

    Added a timeseries for revenue for each species across all ports

--- species_revenue.R
+++ species_revenue.R
@@ -36,12 +36,24 @@ for (s in species) {
   species_timeseries(s)
 }
 
-# Correlation matrix for all fisheries by year & port -------------------------
+# Timeseries of revenue for each species across all ports ---------------------
 fish_sums <- group_by(port_landings, year, port, fishery) %>% 
   summarize(revenue = sum(ex.vessel_revenue))
 fish_sums <- fish_sums %>% drop_na()
 fish_sums2 <-  group_by(fish_sums, year, fishery) %>% 
   summarize(revenue = sum(revenue))
+
+fish_revenue_sum <- ggplot(fish_sums2, aes(x = year, y = revenue)) +
+  theme_bw() +
+  geom_line(size=1.5) +  
+  ylab("revenue") + xlab(" ") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  facet_wrap(~fishery, ncol = 6)
+
+ggsave(filename=paste("Figures/Species revenue/allfisheries_revenue.pdf", sep=""), 
+       plot=fish_revenue_sum, width=600, height=500, units="mm", dpi=300)
+
+# Correlation matrix for all fisheries by year & port -------------------------
 fish_sums3 <- dcast(fish_sums2, year ~ fishery)
 
 fish_cor <- cor(fish_sums3[2:37])

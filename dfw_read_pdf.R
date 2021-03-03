a9755c8fdb5f9c2a3f86f28e164e09ec36886f3a
Sophia Wassermann <hi@sowasser.com>
2021-03-03 15:56:45 +0000

    Tried multiple options in R....nothing is working well

--- dfw_read_pdf.R
+++ dfw_read_pdf.R
@@ -2,23 +2,35 @@ library(pdftools)
 library(tidyverse)
 
 # Read in pdf
-pdf <- pdf_text("DFW pdf data/Table 8.pdf") %>% readr::read_lines()
+pdf <- pdf_text("DFW pdf data/Table 8.pdf") %>% 
+  str_split("\n")
+  # readr::read_lines() %>%  # separate lines
+  # read_fwf(fwf_empty(.)) %>%  # read as fixed-width file
+  # mutate_at(-1, parse_number) %>%  # make numbers numbers
+  # mutate(X1 = sub(',', '', X1, fixed = TRUE))  # remove commas in numbers NOT WORKING
 
 columns <- c("species", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
              "Sep", "Oct", "Nov", "Dec", "landings")
 
+# Remove page headers
+for(i in 1:7) {
+  pdf[[i]] <- pdf[[i]][-1:-5]
+}
+
+
+df2 <- as.data.frame(lapply(pdf, trimws), stringsAsFactors = FALSE)
+
 # Remove lines you don't want
-pdf2 <- pdf[-c(1:5, 14,            # Page 1
-               41:46, 62,          # Page 2
-               83:88, 96, 108,     # Page 3
-               125:130, 139, 162,  # Page 4
-               167:172,            # Page 5
-               208:213, 228,       # Page 6
-               249:254, 260, 270, 282, 284, 286:292  # Page 7
-               )]
+pdf2 <- pdf[-c(1:5, 41:45, 82:86, 94, 106, 123:127, 164:168, 204:208, 244:248, 254, 264, 278, 280:285), ]
 
 # Attempt to break each row (an entire string) into separate strings
 pdf3 <- pdf2 %>% str_squish() %>% strsplit(split = " ")
 
 # Convert to dataframe
-df <- plyr::ldply(pdf3)
+df <- plyr::ldply(pdf2)
+
+library(tabulizer)
+library(dplyr)
+
+location <- "DFW pdf data/Table 8.pdf"
+out <- extract_table(location)

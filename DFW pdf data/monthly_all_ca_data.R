a897ce3779f4f61fd1ae73fd868890be0936094c
Sophia Wassermann <hi@sowasser.com>
2021-03-04 17:17:16 +0000

    Moved file, added year column manually, and attempted to write files
    For some reason, the files aren't writing correctly. The dataframes are fine, but they're being written as a series of numbers in one column and 2019 repeated in the 2nd column.

--- DFW pdf data/monthly_all_ca_data.R
+++ DFW pdf data/monthly_all_ca_data.R
@@ -3,7 +3,6 @@
 
 library(stringr)
 
-# Read in and manipulate data -------------------------------------------------
 # Read in each file with a unique name matching the year
 names <- c("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
            "11", "12", "13", "14", "15", "16", "17", "18", "19")
@@ -35,3 +34,32 @@ m16$Species <- str_replace_all(m16$Species, "[[:punct:]]", "")
 m17$Species <- str_replace_all(m17$Species, "[[:punct:]]", "")
 m18$Species <- str_replace_all(m18$Species, "[[:punct:]]", "")
 m19$Species <- str_replace_all(m19$Species, "[[:punct:]]", "")
+
+# Add a column with the year of the data
+# TODO: figure out how to do this iteratively/functionally
+m00[, "year"] <- rep(2000, length(m00$Species))
+m01[, "year"] <- rep(2001, length(m01$Species))
+m02[, "year"] <- rep(2002, length(m02$Species))
+m03[, "year"] <- rep(2003, length(m03$Species))
+m04[, "year"] <- rep(2004, length(m04$Species))
+m05[, "year"] <- rep(2005, length(m05$Species))
+m06[, "year"] <- rep(2006, length(m06$Species))
+m07[, "year"] <- rep(2007, length(m07$Species))
+m08[, "year"] <- rep(2008, length(m08$Species))
+m09[, "year"] <- rep(2009, length(m09$Species))
+m10[, "year"] <- rep(2010, length(m10$Species))
+m11[, "year"] <- rep(2011, length(m11$Species))
+m12[, "year"] <- rep(2012, length(m12$Species))
+m13[, "year"] <- rep(2013, length(m13$Species))
+m14[, "year"] <- rep(2014, length(m14$Species))
+m15[, "year"] <- rep(2015, length(m15$Species))
+m16[, "year"] <- rep(2016, length(m16$Species))
+m17[, "year"] <- rep(2017, length(m17$Species))
+m18[, "year"] <- rep(2018, length(m18$Species))
+m19[, "year"] <- rep(2019, length(m19$Species))
+
+# Write new .csv files with these changes
+for(i in names) {
+  name <- paste("m", i, sep = "")
+  write.csv(name, (paste("Data/DFW all CA final/m", i, ".csv", sep = "")))
+}

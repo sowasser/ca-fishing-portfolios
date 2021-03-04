1d51ee4d6d63de30e21891065718073264edb2bc
Sophia Wassermann <hi@sowasser.com>
2021-03-04 16:45:10 +0000

    Bleh. Manually removed punctuation from species column
    I have been defeated.

--- monthly_all_ca.R
+++ monthly_all_ca.R
@@ -3,6 +3,7 @@
 
 library(stringr)
 
+# Read in and manipulate data -------------------------------------------------
 # Read in each file with a unique name matching the year
 names <- c("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
            "11", "12", "13", "14", "15", "16", "17", "18", "19")
@@ -13,15 +14,24 @@ for(i in names) {
 }
 
 # Remove weird periods from the species name list
-vectors <- c(m00$Species, m01$Species, m02$Species, m03$Species, m04$Species, 
-             m05$Species, m06$Species, m07$Species, m08$Species, m09$Species, 
-             m10$Species, m11$Species, m12$Species, m13$Species, m14$Species, 
-             m15$Species, m16$Species, m17$Species, m18$Species, m19$Species)
-
-lapply(vectors, function(x)x <- str_replace_all(x, "[[:punct:]]", ""))
-
-for(i in vectors) {
-  i <- str_replace_all(i, "[[:punct:]]", "")
-}
-
+# TODO: figure out how to do this iteratively/functionally
+m00$Species <- str_replace_all(m00$Species, "[[:punct:]]", "")
+m01$Species <- str_replace_all(m01$Species, "[[:punct:]]", "")
+m02$Species <- str_replace_all(m02$Species, "[[:punct:]]", "")
+m03$Species <- str_replace_all(m03$Species, "[[:punct:]]", "")
+m04$Species <- str_replace_all(m04$Species, "[[:punct:]]", "")
+m05$Species <- str_replace_all(m05$Species, "[[:punct:]]", "")
+m06$Species <- str_replace_all(m06$Species, "[[:punct:]]", "")
+m07$Species <- str_replace_all(m07$Species, "[[:punct:]]", "")
+m08$Species <- str_replace_all(m08$Species, "[[:punct:]]", "")
+m09$Species <- str_replace_all(m09$Species, "[[:punct:]]", "")
+m10$Species <- str_replace_all(m10$Species, "[[:punct:]]", "")
+m11$Species <- str_replace_all(m11$Species, "[[:punct:]]", "")
+m12$Species <- str_replace_all(m12$Species, "[[:punct:]]", "")
+m13$Species <- str_replace_all(m13$Species, "[[:punct:]]", "")
 m14$Species <- str_replace_all(m14$Species, "[[:punct:]]", "")
+m15$Species <- str_replace_all(m15$Species, "[[:punct:]]", "")
+m16$Species <- str_replace_all(m16$Species, "[[:punct:]]", "")
+m17$Species <- str_replace_all(m17$Species, "[[:punct:]]", "")
+m18$Species <- str_replace_all(m18$Species, "[[:punct:]]", "")
+m19$Species <- str_replace_all(m19$Species, "[[:punct:]]", "")

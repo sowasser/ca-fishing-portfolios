d0d8ea3804a6aea8a880222605bc49f183bda597
Sophia Wassermann <hi@sowasser.com>
2021-03-05 14:57:06 +0000

    Subset the data for the years in the stable period to find monthly means
    Still need to figure out whether to subset or combine all Rockfish & Thornyhead species

--- dfw_monthly_all_ca.R
+++ dfw_monthly_all_ca.R
@@ -5,6 +5,7 @@
 
 library(stringr)
 library(dplyr)
+library(reshape2)
 
 # Read in each file with a unique name matching the year ----------------------
 names <- c("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
@@ -19,7 +20,7 @@ for(n in names) {
                   colClasses = c("character", rep("numeric", 14))))
 }
 
-# Subset dataframes by species of interest
+# Subset dataframes by species of interest ------------------------------------
 dfs <- list(m00, m01, m02, m03, m04, m05, m06, m07, m08, m09, m10, m11, m12,
             m13, m14, m15, m16, m17, m18, m19)
 
@@ -46,3 +47,16 @@ pelagics <- rbind(sardine, pac_mackerel, jack_mackerel, anchovy)
 # Create dataframe of all species of interest
 soi <- rbind(halibut, crab, lobster, squid, albacore, prawn, urchin, sablefish,
              rockfish, swordfish, thornyhead, pelagics)
+
+# Monthly means for years in the 'stable period' - 2009-2014 ------------------
+# Filter for 2009-2014
+soi_stable <- soi %>% filter(between(year, 2009, 2014))
+soi_stable <- soi_stable[, -14]  # remove total landings column
+
+# Reshape data & find mean
+soi_stable_means <- melt(soi_stable, id.vars = c("Species", "year"))
+colnames(soi_stable_means) <- c("species", "year", "month", "landings")
+  
+soi_stable_means <- soi_stable_means %>%
+  group_by(species, month) %>% 
+  summarize(landings = mean(landings))

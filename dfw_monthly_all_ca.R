41f221d25bbdced999f847a1446fbff17548fb66
Sophia Wassermann <hi@sowasser.com>
2021-03-05 17:59:13 +0000

    Combined fish categories w/ many species & found stable period means

--- dfw_monthly_all_ca.R
+++ dfw_monthly_all_ca.R
@@ -24,25 +24,69 @@ for(n in names) {
 dfs <- list(m00, m01, m02, m03, m04, m05, m06, m07, m08, m09, m10, m11, m12,
             m13, m14, m15, m16, m17, m18, m19)
 
-halibut <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Halibut California"))) %>% bind_rows
-crab <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Dungeness"))) %>% bind_rows
-lobster <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Lobster"))) %>% bind_rows
-squid <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Squid market"))) %>% bind_rows
-albacore <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "albacore"))) %>% bind_rows
-prawn <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Prawn spot"))) %>% bind_rows
-urchin <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Sea urchin red"))) %>% bind_rows
-sablefish <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Sablefish"))) %>% bind_rows
-rockfish <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Rockfish"))) %>% bind_rows
-swordfish <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Swordfish"))) %>% bind_rows
-thornyhead <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Thornyhead"))) %>% bind_rows
-# TODO: determine if rockfish & thornyhead should be further sub-sampled or combined
+halibut <- lapply(dfs, function(x) x %>% 
+                    filter(str_detect(Species, "Halibut California"))) %>% bind_rows
+crab <- lapply(dfs, function(x) x %>% 
+                 filter(str_detect(Species, "Dungeness"))) %>% bind_rows
+lobster <- lapply(dfs, function(x) x %>% 
+                    filter(str_detect(Species, "Lobster"))) %>% bind_rows
+squid <- lapply(dfs, function(x) x %>% 
+                  filter(str_detect(Species, "Squid market"))) %>% bind_rows
+albacore <- lapply(dfs, function(x) x %>% 
+                     filter(str_detect(Species, "albacore"))) %>% bind_rows
+prawn <- lapply(dfs, function(x) x %>% 
+                  filter(str_detect(Species, "Prawn spot"))) %>% bind_rows
+urchin <- lapply(dfs, function(x) x %>% 
+                   filter(str_detect(Species, "Sea urchin red"))) %>% bind_rows
+sablefish <- lapply(dfs, function(x) x %>% 
+                      filter(str_detect(Species, "Sablefish"))) %>% bind_rows
+swordfish <- lapply(dfs, function(x) x %>% 
+                      filter(str_detect(Species, "Swordfish"))) %>% bind_rows
 
-# Coastal pelagic species gathered from NOAA fisheries
-sardine <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Sardine"))) %>% bind_rows
-pac_mackerel <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Mackerel Pacific"))) %>% bind_rows
-jack_mackerel <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Mackerel jack"))) %>% bind_rows
-anchovy <- lapply(dfs, function(x) x %>% filter(str_detect(Species, "Anchovy northern"))) %>% bind_rows
-pelagics <- rbind(sardine, pac_mackerel, jack_mackerel, anchovy)
+# TODO: determine if rockfish & thornyhead should be further sub-sampled or combined
+# Pull out and combine shelf-slope rockfish
+rockfish_shelf <- lapply(dfs, function(x) x %>% 
+                           filter(str_detect(Species, "Rockfish group shelf"))) %>% bind_rows
+rockfish_slope <- lapply(dfs, function(x) x %>% 
+                           filter(str_detect(Species, "Rockfish group slope"))) %>% bind_rows
+
+rockfish <- rbind(rockfish_shelf, rockfish_slope)[, -1]  # remove species column to find sums
+rockfish <- rockfish %>% group_by(year) %>% summarize(across(January:Landings, sum))  #find sum
+rockfish <- rockfish[, c(2:14, 1)]  # Make year last column
+rockfish <- cbind(rep("Rockfish shelf-slope", length(rockfish$year)), rockfish)
+columns <- c("Species", "January", "February", "March", "April", "May", "June",
+             "July", "August", "September", "October", "November", "December", 
+             "Landings", "year")
+colnames(rockfish) <- columns
+
+thornyhead <- lapply(dfs, function(x) x %>% 
+                       filter(str_detect(Species, "Thornyhead"))) %>% bind_rows
+
+thornyhead <- thornyhead %>% group_by(year) %>% summarize(across(January:Landings, sum))  #find sum
+thornyhead <- thornyhead[, c(2:14, 1)]  # Make year last column
+thornyhead <- cbind(rep("Thornyhead", length(thornyhead$year)), thornyhead)
+columns <- c("Species", "January", "February", "March", "April", "May", "June",
+             "July", "August", "September", "October", "November", "December", 
+             "Landings", "year")
+colnames(thornyhead) <- columns
+
+
+# Combine coastal pelagic species gathered from NOAA fisheries
+sardine <- lapply(dfs, function(x) x %>% 
+                    filter(str_detect(Species, "Sardine"))) %>% bind_rows
+pac_mackerel <- lapply(dfs, function(x) x %>% 
+                         filter(str_detect(Species, "Mackerel Pacific"))) %>% bind_rows
+jack_mackerel <- lapply(dfs, function(x) x %>% 
+                          filter(str_detect(Species, "Mackerel jack"))) %>% bind_rows
+anchovy <- lapply(dfs, function(x) x %>% 
+                    filter(str_detect(Species, "Anchovy northern"))) %>% bind_rows
+
+pelagics <- rbind(sardine, pac_mackerel, jack_mackerel, anchovy)[, -1]  # remove species column
+pelagics <- pelagics %>% group_by(year) %>% summarize(across(January:Landings, sum))  #find sum
+pelagics <- pelagics[, c(2:14, 1)]  # Make year last column
+pelagics <- cbind(rep("Pelagics coastal", length(pelagics$year)), pelagics)
+colnames(pelagics) <- columns
+
 
 # Create dataframe of all species of interest
 soi <- rbind(halibut, crab, lobster, squid, albacore, prawn, urchin, sablefish,
@@ -51,12 +95,11 @@ soi <- rbind(halibut, crab, lobster, squid, albacore, prawn, urchin, sablefish,
 # Monthly means for years in the 'stable period' - 2009-2014 ------------------
 # Filter for 2009-2014
 soi_stable <- soi %>% filter(between(year, 2009, 2014))
-soi_stable <- soi_stable[, -14]  # remove total landings column
 
-# Reshape data & find mean
-soi_stable_means <- melt(soi_stable, id.vars = c("Species", "year"))
-colnames(soi_stable_means) <- c("species", "year", "month", "landings")
-  
-soi_stable_means <- soi_stable_means %>%
-  group_by(species, month) %>% 
-  summarize(landings = mean(landings))
+# Find monthly mean for each year
+soi_stable <- soi_stable[, -15]  # remove year column
+soi_stable$Species <- str_trim(soi_stable$Species, side = "both")  # Remove extra white spaces
+
+soi_means <- soi_stable %>% 
+  group_by(Species) %>% 
+  summarize(across(January:Landings, mean))

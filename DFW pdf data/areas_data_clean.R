1e9b20fdd26da9d646e8a8d8c64624c52e33d6ca
Sophia Wassermann <hi@sowasser.com>
2021-03-10 16:16:56 +0000

    More functional data cleaning & export of one file per area
    May be a good idea eventually to also create dataframes & export per year, with all areas combined.

--- DFW pdf data/areas_data_clean.R
+++ DFW pdf data/areas_data_clean.R
@@ -9,24 +9,26 @@ library(tidyverse)
 # For Bodega Bay --------------------------------------------------------------
 # Read in each file as a dataframe within a list
 files <- list.files(path = "DFW pdf data/updated/areas/Bodega Bay/", pattern = "*.csv")
+
 bb <- lapply(files, function(x) 
   read.csv(paste("DFW pdf data/updated/areas/Bodega Bay/", x, sep = "")))
 
-# TODO: add 2015-2017 when pdf download issue is fixed
-data_years <- c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 
-                2010, 2011, 2012, 2013, 2014, 2018, 2019)
-
-# Function to fix weird formatting issues
-data_clean <- function(files, years) {
-  area <- list()
-  for(x in files) {
-    for(year in years) {
-      x$Species <- str_replace_all(x$Species, "[[:punct:]]", "")
-      x[, "year"] <- rep(year, length(x$Species))
-      append(area, x)
-    }
-  }
-  return(area)
+# Function to remove weird punctuation from species column
+data_clean <- function(file) {
+  file$Species <- str_replace_all(file$Species, "[[:punct:]]", "")
+  return(file)
 }
 
-bodega_bay <- data_clean(bb, data_years)
+bb_clean <- lapply(bb, data_clean)
+
+# Add a column with the year of the data
+# TODO: add 2015-2017 when pdf download issue is fixed
+years <- c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 
+           2011, 2012, 2013, 2014, 2018, 2019)
+
+bb_years <- map2(bb_clean, years, ~cbind(.x, year = .y))
+
+# Combine all dataframes together into one for the area & write new .csv
+bodega_bay <- bind_rows(bb_years)
+
+write.csv(bodega_bay, "Data/DFW areas/bodega_bay.csv", row.names = FALSE)

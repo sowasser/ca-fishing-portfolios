11705b73ad49d105b026f401f0e9082b4ba812ac
Sophia Wassermann <hi@sowasser.com>
2021-03-11 13:26:21 +0000

    Data read & cleaning now one function to be applied to each area
    This is my first big R function with nested functions and everything!

--- DFW pdf data/areas_data_clean.R
+++ DFW pdf data/areas_data_clean.R
@@ -6,30 +6,36 @@
 library(stringr)
 library(tidyverse)
 
-# For Bodega Bay --------------------------------------------------------------
-# Read in each file as a dataframe within a list
-files <- list.files(path = "DFW pdf data/updated/areas/Bodega Bay/", pattern = "*.csv")
-
-bb <- lapply(files, function(x) 
-  read.csv(paste("DFW pdf data/updated/areas/Bodega Bay/", x, sep = "")))
-
-# Function to remove weird punctuation from species column
-data_clean <- function(file) {
-  file$Species <- str_replace_all(file$Species, "[[:punct:]]", "")
-  return(file)
-}
-
-bb_clean <- lapply(bb, data_clean)
-
-# Add a column with the year of the data
+# Function for reading in and manipulating the data ---------------------------
+# First need to define the years of the data to be added later
 # TODO: add 2015-2017 when pdf download issue is fixed
 years <- c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 
            2011, 2012, 2013, 2014, 2018, 2019)
 
-bb_years <- map2(bb_clean, years, ~cbind(.x, year = .y))
+read_and_clean <- function(area) {
+  # Read in and clean all of the files for an area. Process:
+  # 1. Read in data from the sub-directory for that area
+  # 2. Remove all unneccessary punctuation from the "Species" column
+  # 3. Add a column with the year of each dataframe, 
+  # 4. Combine all years together & add a column with the area name
+  files <- list.files(path = paste("DFW pdf data/updated/areas/", area, "/", sep = ""), 
+                     pattern = "*.csv")
+  df <- lapply(files, function(x) 
+    read.csv(paste("DFW pdf data/updated/areas/", area, "/", x, sep = "")))
+  
+  data_clean <- function(file) {
+    file$Species <- str_replace_all(file$Species, "[[:punct:]]", "")
+    return(file)
+  }
+  df_clean <- lapply(df, data_clean)
+  
+  df_years <- map2(df_clean, years, ~cbind(.x, year = .y))
+  df_final <- bind_rows(df_years)
+  df_final[, "area"] <- rep(area, length(df_final$Species))
+  return(df_final)
+}
 
-# Combine all dfs together into one, add column with area name, write new .csv
-bodega_bay <- bind_rows(bb_years)
-bodega_bay[, "area"] <- rep("Bodega Bay", length(bodega_bay$Species))
-
+# Apply function to each of the areas & write new .csv file -------------------
+bodega_bay <- read_and_clean("Bodega Bay")
 write.csv(bodega_bay, "Data/DFW areas/bodega_bay.csv", row.names = FALSE)
+

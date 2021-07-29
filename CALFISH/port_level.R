ecec16cd005e26884ae008137c839301c2caf3b0
Sophia Wassermann <hi@sowasser.com>
2021-07-29 19:27:12 +0000

    Accessed data through r package wcfish & pulled out monthly data

--- CALFISH/port_level.R
+++ CALFISH/port_level.R
@@ -2,7 +2,17 @@
 # data from the CALFISH database:
 # https://datadryad.org/stash/landing/show?id=doi%3A10.25349%2FD9M907
 
-calfish_original <- read.csv("Data/CALFISH_port.csv")
+# Also accessed through wcfish package:
+# https://github.com/cfree14/wcfish/
+library(wcfish)
 
+# Read in annual port-level data
+port_original <- read.csv("Data/CALFISH_port.csv")
 # Select columns of interest
-calfish <- calfish_original[, c("port", "type", "year", "comm_name", "value_usd", "landings_lb")]
+port <- port_original[, c("port", "type", "year", "comm_name", "value_usd",
+                          "landings_lb")]
+
+# Import monthly landings data by port complex from wcfish
+monthly_original <- swfsc
+monthly <- monthly_original[, c("year", "month", "port_complex", 
+                                "comm_name_orig", "landings_lb")]
